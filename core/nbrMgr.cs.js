09f4cf173c7cc1f4e2263b84ff36161620455a27
Sylvain Plessis <sylvain.plessis@gmail.com>
2016-01-08 11:27:38 +0100

    better management of distance for ticks

--- core/nbrMgr.cs.js
+++ core/nbrMgr.cs.js
@@ -6,35 +6,99 @@ var max   = Math.max;
 var abs   = Math.abs;
 var LN10  = Math.LN10;
 
+
+var m = {};
+
 var firstDigit = function(r){
-	var str = '' + r;
+	var res = r * pow(10,-m.orderMag(r));
+	var str = '' + res;
 	var out = str[0] || 0;
 	return Number(out);
 };
 
-var m = {};
+var roundMe = function(min,max){
+	var valOrder = m.orderMag(max);
+	var val = firstDigit(max) * pow(10,valOrder);
+	var distOrd = m.orderMag(m.distance(max - min));
+
+	var valid = (cand) => {
+		return cand >= min && cand <= max;
+	};
+
+	if(!valid(val)){
+		var cand = val;
+		if(distOrd < valOrder){
+			var newOrder = valOrder;
+			var prev = Math.floor(val * pow(10,-newOrder)) * pow(10,newOrder);
+			while(!valid(cand)){
+				newOrder--;
+				cand = prev;
+				var step = pow(10,newOrder);
+				while(!valid(cand)){
+					cand -= step;
+					if(cand < prev){
+						break;
+					}
+				}
+				prev = cand;
+			}
+		}else{ // distOrd === valOrder ???
+			return null;
+		}
+	}
+	return val;
+};
 
 // distance methods
 m.orderMag = function(r){
 	if(r < 0){
 		r = -r;
 	}
-	return floor( log(r) / LN10);
+	return (r === 0) ? 0 : floor( log(r) / LN10);
 };
 
-m.orderMagValue = m.orderMagDist = function(r){
-	return pow(10,m.orderMag(r));
+m.orderMagValue = m.orderMagDist = function(max,min){
+
+	var valord = Math.max(m.orderMag(max), m.orderMag(min));
+	var distord = m.orderMag(m.distance(max,min));
+
+	// zero case treated right away
+	if(min * max < 0 && valord < distord ){
+		return 0;
+	}
+	var absMin = max < 0 ? Math.abs(max) : min;
+	var absMax = max < 0 ? Math.abs(min) : max;
+	var fac = max < 0 ? -1 : 1;
+	return fac * roundMe(absMin,absMax);
 };
 
 m.roundUp = function(r){
-	var step = 5 * pow(10,m.orderMag(r) - 1);
-	var cand = pow(10,m.orderMag(r)) * firstDigit(r);
-	while(cand < r){cand += step;}
+	var step = (val) => {
+		switch(firstDigit(val)){
+			case 2:
+				return 5 * pow(10,m.orderMag(cand));
+			default:
+				return 2 * cand;
+		}
+		
+	};
+	var cand = pow(10,m.orderMag(r));
+	while(cand <= r){
+		cand = step(cand);
+	}
+
+	var test = cand * pow(10,-m.orderMag(cand)); // between 0 and 1
+	if(test > 6){
+		cand = pow(10,m.orderMag(cand) + 1);
+	}
 	return cand;
 };
 
 m.roundDown = function(r){
-	return firstDigit(r) * pow(10,m.orderMag(r));
+	var step = 5 * pow(10,m.orderMag(r) - 1);
+	var cand = firstDigit(r) * pow(10,m.orderMag(r));
+	while(cand >= r){cand -= step;}
+	return cand;
 };
 
 m.multiply = function(d,f){
