ccb4222f2881c2aab40810334d8a6faa2f0e7a0c
Sylvain Plessis <sylvain.plessis@gmail.com>
2016-01-22 15:05:05 +0100

    Color management and shade management generalized

--- core/colorMgr.cs.js
+++ core/colorMgr.cs.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 var utils = require('./utils.cs.js');
 
 var shader = {};
-shader.color = function(colors,f){
+shader.color = function(options,f){
 
 		var toRGB= function(str,w){
 			return {
@@ -25,20 +25,24 @@ shader.color = function(colors,f){
 		};
 
 		var coord = (utils.isArray(f)) ? f : [f, 1 - f];
-		return toString(_.reduce(colors, (memo, col, idx) => addRGB(memo,toRGB(col,coord[idx])), {R:0, G:0, B:0}));
+		return toString(_.reduce(options.colors, (memo, col, idx) => addRGB(memo,toRGB(col,coord[idx])), {R:0, G:0, B:0}));
 	
 };
 
 shader.shade = function(options,f){
-	return f;
+	var val = f;
+	if(!!options.shadings && options.shadings.length >= 2){
+		val = options.shadings[0] + (options.shadings[1] - options.shadings[0]) * f;
+	}
+	return val;
 };
 
 var compute = function(mgr){
 	switch(mgr.computation){
 		case 'by index':
-			return shader[mgr.type](mgr.options.colors,mgr.index / mgr.N);
+			return shader[mgr.type](mgr.options,mgr.index / mgr.N);
 		case 'explicit':
-			return shader[mgr.type](mgr.options.colors,mgr.factor[mgr.index]);
+			return shader[mgr.type](mgr.options,mgr.factor[mgr.index]);
 		case 'by function':
 			return !!mgr.shadeFunction ? mgr.shadeFunction(mgr.point) : 'black';
 	}
