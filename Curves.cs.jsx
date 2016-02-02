ae18ee18e96fbdd1884de95b2f26542b55fd1386
Sylvain Plessis <sylvain.plessis@gmail.com>
2016-02-02 23:09:15 +0100

    Forgotten high-level file

---
+++ Curves.cs.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var _ = require('underscore');
+var grapher = require('./graphs/grapher.cs.jsx');
+var imUtils = require('./core/im-utils.cs.js');
+
+/*
+	{
+		curves: [{
+			key: '', 
+			points: [{}],
+			props: {
+			}
+		}]
+	}
+*/
+
+var Curves = React.createClass({
+	
+	shouldComponentUpdate: function(props){
+		return !imUtils.isEqual(props,this.props);
+	},
+
+	render: function(){
+		return <g>{_.map(this.props.curves, (curve) => {return grapher(curve.type,curve);})}</g>;
+	}
+
+});
+
+module.exports = Curves;
